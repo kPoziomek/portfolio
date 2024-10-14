b322d66a92eae1204414150da0d07b5c8c9c53fe
Krzysztof Poziomek <k.poziomek@selleo.com>
2024-10-14 12:48:18 +0200

    add animation to welcome

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -1,15 +1,28 @@
 import CustomButton from "~/components/ui/CustomButton";
 import { PiHandEyeDuotone } from "react-icons/pi";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import cx from "~/utils/cx";
 import { IconContext } from "~/components/context/iconContext";
 
 export default function Welcome() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 500); // Start animation after 500ms
+  }, []);
   const { size, color } = useContext(IconContext);
 
   return (
     <section
       id="welcome"
-      className="h-[100vh] bg-white flex justify-center relative w-full overflow-hidden pb-10 rounded-3xl shadow-md"
+      className={cx(
+        `h-[100vh] bg-white flex justify-center relative w-full overflow-hidden pb-10 rounded-3xl shadow-md 
+        transition-all duration-1000 ease-in-out ${
+          isVisible ? "opacity-100" : "opacity-0"
+        } delay-100`
+      )}
     >
       <div className="overlay1" />
       <div className="overlay2" />
@@ -18,22 +31,30 @@ export default function Welcome() {
           <div className="sectionTitle">
             <div className="text-center pb-10">
               <p className="flex justify-center items-center gap-2.5">
-                Hi I'm Poziom{" "}
+                Hi I&#39;m Poziom{" "}
                 <div>
                   <PiHandEyeDuotone size={size} color={color} />
                 </div>
               </p>
               <h1 className="text-h1-mobile md:text-h1-tablet lg:text-h1-desktop">
-                I'm a WebDeveloper{" "}
+                I&#39;m a WebDeveloper{" "}
               </h1>
             </div>
             <div className="md:container sectionButtons flex flex-col md:flex-row justify-center items-center gap-2">
-              <CustomButton to="#work" title="My work" className="md:w-1/5" />
+              <CustomButton
+                to="#work"
+                title="My work"
+                className={`md:w-1/5 transition-all duration-500 ease-in-out ${
+                  isVisible ? "opacity-100" : "opacity-0"
+                } delay-500`}
+              />
               <CustomButton
                 to="#about"
                 title="About me"
                 type="secondary"
-                className="md:w-1/5"
+                className={`md:w-1/5 transition-all duration-500 ease-in-out ${
+                  isVisible ? "opacity-100" : "opacity-0"
+                } delay-300`}
               />
             </div>
           </div>
